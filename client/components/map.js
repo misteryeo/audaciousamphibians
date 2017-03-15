44f2c6b672cab63d469cc179c5dcd848338d207a
Andy Yeo <andy.yeo@gmail.com>
2017-03-14 19:24:30 -0700

    Add navbar component.

--- client/components/map.js
+++ client/components/map.js
@@ -1,1 +1,17 @@
-map.js
+import React from 'react'
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>
+        {/* Insert map */}
+      </div>
+    )
+  }
+}
+
+export default Map
