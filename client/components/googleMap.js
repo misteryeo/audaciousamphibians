95fbf7caeeeb1856e38c8e4319328f6485485a83
Keith Alpichi <keithalpichi@gmail.com>
2017-03-21 11:26:37 -0700

    add GoogleMap wrapper component

---
+++ client/components/googleMap.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { GoogleMap, Marker, withGoogleMap, DirectionsRenderer } from "react-google-maps"
+
+const DrivingGoogleMap = props => (
+  <GoogleMap
+  >
+    {/*props.markers.map(marker => (
+      <Marker
+        {...marker}
+      />
+    ))
+    */}
+    {props.directions && <DirectionsRenderer directions={props.directions} />}
+  </GoogleMap>
+)
+
+export default withGoogleMap(DrivingGoogleMap)
