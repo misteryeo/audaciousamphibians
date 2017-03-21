ce62fe49329deb5e1826b4ceda499942ceb631ac
Enoch Kim <enochkim91@gmail.com>
2017-03-21 14:45:59 -0700

    Moves dummy data to trippage, passes down to POI, starts building poiEntry component

--- client/components/poi.js
+++ client/components/poi.js
@@ -1,46 +1,5 @@
 import React from 'react'
-
-const dummyData = {
-  results: [
-    {
-      geometry : {
-        location: {
-          lat: -33.870775,
-          lng: 151.199025
-        }
-      },
-      icon: "http://maps.gstatic.com/mapfiles/place_api/icons/travel_agent-71.png",
-      name: 'Rhythmboat Cruises',
-      place_id: 'ChIJyWEHuEmuEmsRm9hTkapTCrk',
-      types: [ "travel_agency", "restaurant", "food", "establishment" ]
-    },
-    {
-      geometry : {
-        location: {
-          lat: -33.866891,
-          lng: 151.200814
-        }
-      },
-      icon: "http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
-      name: "Private Charter Sydney Habour Cruise",
-      place_id: "ChIJqwS6fjiuEmsRJAMiOY9MSms",
-      types: [ "restaurant", "food", "establishment" ]
-    },
-    {
-      geometry : {
-        location: {
-          lat: -33.867591,
-          lng: 151.201196
-        }
-      },
-      icon: "http://maps.gstatic.com/mapfiles/place_api/icons/travel_agent-71.png",
-      name: "Australian Cruise Group",
-      place_id: "ChIJrTLr-GyuEmsRBfy61i59si0",
-      types: [ "travel_agency", "restaurant", "food", "establishment" ]
-    }
-  ],
-  status: 'OK'
-}
+import POIEntry from './poiEntry.js'
 
 class POI extends React.Component {
   constructor(props) {
@@ -52,7 +11,11 @@ class POI extends React.Component {
         <input type="text" placeholder="Start"></input>
         <input type="text" placeholder="Finish"></input>
         {/*}<Link className="search" to="/trippage">Search</Link>*/}
-        Here is the POI component
+        {
+          this.props.markers.map((entry) => {
+            return (<POIEntry {...entry}/>)
+          })
+        }
         <button onClick={this.requestRoute}>Search</button>
       </div>
     )
---
+++ client/components/poiEntry.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const POIEntry = (props) => {
+  
+}
+
+export default POIEntry
