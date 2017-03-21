5159cd8079fb96cdf81a08644abfcf5202943561
Enoch Kim <enochkim91@gmail.com>
2017-03-21 14:18:45 -0700

    Adds dummy data to use for POI list

--- client/components/poi.js
+++ client/components/poi.js
@@ -1,5 +1,47 @@
 import React from 'react'
 
+const dummyData = {
+  results: [
+    {
+      geometry : {
+        location: {
+          lat: -33.870775,
+          lng: 151.199025
+        }
+      },
+      icon: "http://maps.gstatic.com/mapfiles/place_api/icons/travel_agent-71.png",
+      name: 'Rhythmboat Cruises',
+      place_id: 'ChIJyWEHuEmuEmsRm9hTkapTCrk',
+      types: [ "travel_agency", "restaurant", "food", "establishment" ]
+    },
+    {
+      geometry : {
+        location: {
+          lat: -33.866891,
+          lng: 151.200814
+        }
+      },
+      icon: "http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+      name: "Private Charter Sydney Habour Cruise",
+      place_id: "ChIJqwS6fjiuEmsRJAMiOY9MSms",
+      types: [ "restaurant", "food", "establishment" ]
+    },
+    {
+      geometry : {
+        location: {
+          lat: -33.867591,
+          lng: 151.201196
+        }
+      },
+      icon: "http://maps.gstatic.com/mapfiles/place_api/icons/travel_agent-71.png",
+      name: "Australian Cruise Group",
+      place_id: "ChIJrTLr-GyuEmsRBfy61i59si0",
+      types: [ "travel_agency", "restaurant", "food", "establishment" ]
+    }
+  ],
+  status: 'OK'
+}
+
 class POI extends React.Component {
   constructor(props) {
     super(props)
