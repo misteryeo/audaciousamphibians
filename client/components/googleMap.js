268ecc5bec6c54088e383a780ba3fcab361acd5f
Keith Alpichi <keithalpichi@gmail.com>
2017-03-23 10:19:00 -0700

    add in marker props with onclick handler and key prop

--- client/components/googleMap.js
+++ client/components/googleMap.js
@@ -4,12 +4,14 @@ import { GoogleMap, Marker, withGoogleMap, DirectionsRenderer } from 'react-goog
 const DrivingGoogleMap = props => (
   <GoogleMap
   >
-    {/*props.markers.map(marker => (
+    {props.markers && props.markers.map(marker => (
       <Marker
         {...marker}
+        onClick={() => console.log(marker.place.placeId)}
+        key={marker.place.placeId}
       />
     ))
-    */}
+    }
     {props.directions && <DirectionsRenderer directions={props.directions} />}
   </GoogleMap>
 )
