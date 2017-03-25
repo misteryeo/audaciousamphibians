37fcdd2242c05b46cdacc2c0da33d32215063189
Andy Yeo <andy.yeo@gmail.com>
2017-03-24 17:12:09 -0700

    Fix merge conflicts

--- client/components/landingpage.js
+++ client/components/landingpage.js
@@ -1,34 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { PropTypes } from 'react'
+import Search from './search.js'
 
 class LandingPage extends React.Component {
   constructor(props) {
     super(props)
-    this.updateFilters = this.updateFilters.bind(this);
-  }
-
-  updateFilters(){
-    // Get the values of the checkboxes (booleans)
-    var food = this.refs.food.checked; //value of checkbox
-    var attractions = this.refs.attractions.checked; //value of checkbox
-    this.props.setFilters(food, attractions);
-    this.props.setRoute(this.refs.start.value, this.refs.end.value)
-    this.props.history.push('/trip')
   }
 
   render() {
     return(
       <div id="landingpage">
-      <div className="searchinput">
-        <input className="start" ref="start" type="text" placeholder="Start"></input>
-        <input className="finish" ref="end" type="text" placeholder="Finish"></input>
-        <button className="myButton" onClick={this.updateFilters}>GO</button>
-      </div>
-      <div className="checkbox">
-        <input ref="food" type="checkbox"></input>Food
-        <input ref="attractions" type="checkbox"></input>Attractions
-      </div>
+        <Search />
       </div>
     )
   }
