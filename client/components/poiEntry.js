0b0523bb16a6f4282f38cd2776738df90dd3ba49
Keith Alpichi <keithalpichi@gmail.com>
2017-03-27 14:30:06 -0700

    add styling

--- client/components/poiEntry.js
+++ client/components/poiEntry.js
@@ -3,15 +3,15 @@ import React from 'react'
 const POIEntry = (props) => (
   <div className='poiEntries'>
 	  <div onClick={props.setSelectedPOI.bind(null, props)}>
-	    <div className='icon'><img src={props.icon}/> 
+	    <div className='icon'><img src={props.icon}/>
 	    </div>
 	    <div className='title'> {props.name}
 	    </div>
 	  </div>
     <div className='clear'></div>
-    { props.selectedTab === 0 ? 
-    	<div><button onClick={props.addPOI.bind(null, props)}>Add To Trip</button></div> 
-    	: <div><button onClick={props.deletePOI.bind(null, props)}>Remove From Trip</button></div> }
+    { props.selectedTab === 0 ?
+    	<div><button className="poiButtons" onClick={props.addPOI.bind(null, props)}>Add To Trip</button></div>
+    	: <div><button className="poiButtons" onClick={props.deletePOI.bind(null, props)}>Remove From Trip</button></div> }
   </div>
 )
 
